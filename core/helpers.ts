f51109894c5b11107444f88f45e4723e53a9ed9b
baibonjwa <baibonjwa@gmail.com>
2023-12-12 13:59:52 +0800

    Update initialRoot helper method

--- core/helpers.ts
+++ core/helpers.ts
@@ -4,22 +4,22 @@ import {SgfNode} from './types';
 
 const tree: TreeModel = new TreeModel();
 
-const initRootProps = [
-  'FF[4]',
-  'GM[1]',
-  'CA[UTF-8]',
-  'AP[ghostgo:0.1.0]',
-  'SZ[19]',
-  'ST[0]',
-].map(p => RootProp.from(p));
-
-export const initialRoot = () =>
-  tree.parse({
+export const initialRoot = (
+  rootProps = [
+    'FF[4]',
+    'GM[1]',
+    'CA[UTF-8]',
+    'AP[ghostgo:0.1.0]',
+    'SZ[19]',
+    'ST[0]',
+  ]
+) => {
+  return tree.parse({
     id: 'root',
     name: 0,
     index: 0,
     number: 0,
-    rootProps: initRootProps,
+    rootProps: rootProps.map(p => RootProp.from(p)),
     moveProps: [],
     setupProps: [],
     markupProps: [],
@@ -28,6 +28,7 @@ export const initialRoot = () =>
     moveAnnotationProps: [],
     customProps: [],
   });
+};
 
 export function isCharacterInNode(
   sgf: string,
